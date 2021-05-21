26534cd5691e1aa47d267b4d2ac300dbd8dd297c
heiko.rothe <heiko.rothe@idealo.de>
2021-05-21 12:31:35 +0200

    Add tagging support

--- internal/provider/resource_aws_account.go
+++ internal/provider/resource_aws_account.go
@@ -2,6 +2,7 @@ package provider
 
 import (
 	"context"
+	"fmt"
 	"regexp"
 	"sync"
 	"time"
@@ -74,6 +75,12 @@ func resourceAWSAccount() *schema.Resource {
 				Type:        schema.TypeString,
 				Required:    true,
 			},
+			"tags": {
+				Description: "Key-value map of resource tags for the account.",
+				Type:        schema.TypeMap,
+				Optional:    true,
+				Elem:        &schema.Schema{Type: schema.TypeString},
+			},
 			"provisioned_product_name": {
 				Description: "Name of the service catalog product that is provisioned.",
 				Type:        schema.TypeString,
@@ -94,6 +101,7 @@ var accountMutex sync.Mutex
 
 func resourceAWSAccountCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	scconn := m.(*AWSClient).scconn
+	organizationsconn := m.(*AWSClient).organizationsconn
 
 	products, err := scconn.SearchProducts(&servicecatalog.SearchProductsInput{
 		Filters: map[string][]*string{"FullTextSearch": {aws.String("AWS Control Tower Account Factory")}},
@@ -180,9 +188,23 @@ func resourceAWSAccountCreate(ctx context.Context, d *schema.ResourceData, m int
 	d.SetId(*account.RecordDetail.ProvisionedProductId)
 
 	// Wait for the provisioning to finish.
-	diags := waitForProvisioning(name, account.RecordDetail.RecordId, m)
+	record, diags := waitForProvisioning(name, account.RecordDetail.RecordId, m)
 	if diags.HasError() {
 		return diags
+	}
+
+	tags := d.Get("tags").(map[string]interface{})
+	for _, output := range record.RecordOutputs {
+		switch *output.OutputKey {
+		case "AccountId":
+			_, err := organizationsconn.TagResource(&organizations.TagResourceInput{
+				ResourceId: output.OutputValue,
+				Tags:       toOrganizationsTags(tags),
+			})
+			if err != nil {
+				return diag.Errorf("Error tagging account %s: %v", *output.OutputValue, err)
+			}
+		}
 	}
 
 	return resourceAWSAccountRead(ctx, d, m)
@@ -301,61 +323,83 @@ func resourceAWSAccountRead(ctx context.Context, d *schema.ResourceData, m inter
 		return diag.FromErr(err)
 	}
 
+	tags, err := organizationsconn.ListTagsForResource(&organizations.ListTagsForResourceInput{
+		ResourceId: aws.String(accountId),
+	})
+	if err != nil {
+		return diag.Errorf("Error listing tags for resource %s: %v", accountId, err)
+	}
+	if err := d.Set("tags", fromOrganizationTags(tags.Tags)); err != nil {
+		return diag.FromErr(err)
+	}
+
 	return nil
 }
 
 func resourceAWSAccountUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	scconn := m.(*AWSClient).scconn
+	organizationsconn := m.(*AWSClient).organizationsconn
 
-	// Get the name, email, ou and SSO details from the config.
-	name := d.Get("name").(string)
-	email := d.Get("email").(string)
-	ou := d.Get("organizational_unit").(string)
-	sso := d.Get("sso").([]interface{})[0].(map[string]interface{})
+	if d.HasChangeExcept("tags") {
+		// Get the name, email, ou and SSO details from the config.
+		name := d.Get("name").(string)
+		email := d.Get("email").(string)
+		ou := d.Get("organizational_unit").(string)
+		sso := d.Get("sso").([]interface{})[0].(map[string]interface{})
 
-	// Create a new parameters struct.
-	params := &servicecatalog.UpdateProvisionedProductInput{
-		ProvisionedProductId: aws.String(d.Id()),
-		ProvisioningParameters: []*servicecatalog.UpdateProvisioningParameter{
-			{
-				Key:   aws.String("AccountName"),
-				Value: aws.String(name),
+		// Create a new parameters struct.
+		params := &servicecatalog.UpdateProvisionedProductInput{
+			ProvisionedProductId: aws.String(d.Id()),
+			ProvisioningParameters: []*servicecatalog.UpdateProvisioningParameter{
+				{
+					Key:   aws.String("AccountName"),
+					Value: aws.String(name),
+				},
+				{
+					Key:   aws.String("AccountEmail"),
+					Value: aws.String(email),
+				},
+				{
+					Key:   aws.String("SSOUserFirstName"),
+					Value: aws.String(sso["firstname"].(string)),
+				},
+				{
+					Key:   aws.String("SSOUserLastName"),
+					Value: aws.String(sso["lastname"].(string)),
+				},
+				{
+					Key:   aws.String("SSOUserEmail"),
+					Value: aws.String(sso["email"].(string)),
+				},
+				{
+					Key:   aws.String("ManagedOrganizationalUnit"),
+					Value: aws.String(ou),
+				},
 			},
-			{
-				Key:   aws.String("AccountEmail"),
-				Value: aws.String(email),
-			},
-			{
-				Key:   aws.String("SSOUserFirstName"),
-				Value: aws.String(sso["firstname"].(string)),
-			},
-			{
-				Key:   aws.String("SSOUserLastName"),
-				Value: aws.String(sso["lastname"].(string)),
-			},
-			{
-				Key:   aws.String("SSOUserEmail"),
-				Value: aws.String(sso["email"].(string)),
-			},
-			{
-				Key:   aws.String("ManagedOrganizationalUnit"),
-				Value: aws.String(ou),
-			},
-		},
+		}
+
+		accountMutex.Lock()
+		defer accountMutex.Unlock()
+
+		account, err := scconn.UpdateProvisionedProduct(params)
+		if err != nil {
+			return diag.Errorf("Error updating provisioned account %s: %v", name, err)
+		}
+
+		// Wait for the provisioning to finish.
+		_, diags := waitForProvisioning(name, account.RecordDetail.RecordId, m)
+		if diags.HasError() {
+			return diags
+		}
 	}
 
-	accountMutex.Lock()
-	defer accountMutex.Unlock()
+	if d.HasChange("tags") {
+		o, n := d.GetChange("tags")
+		accountId := d.Get("account_id").(string)
 
-	account, err := scconn.UpdateProvisionedProduct(params)
-	if err != nil {
-		return diag.Errorf("Error updating provisioned account %s: %v", name, err)
-	}
-
-	// Wait for the provisioning to finish.
-	diags := waitForProvisioning(name, account.RecordDetail.RecordId, m)
-	if diags.HasError() {
-		return diags
+		if err := updateAccountTags(organizationsconn, accountId, o, n); err != nil {
+			return diag.Errorf("Error updating AWS Organizations Account (%s) tags: %s", accountId, err)
+		}
 	}
 
 	return resourceAWSAccountRead(ctx, d, m)
@@ -378,14 +422,18 @@ func resourceAWSAccountDelete(ctx context.Context, d *schema.ResourceData, m int
 	}
 
 	// Wait for the provisioning to finish.
-	return waitForProvisioning(name, account.RecordDetail.RecordId, m)
+	_, diags := waitForProvisioning(name, account.RecordDetail.RecordId, m)
+	return diags
 }
 
 // waitForProvisioning waits until the provisioning finished.
-func waitForProvisioning(name string, recordID *string, m interface{}) diag.Diagnostics {
+func waitForProvisioning(name string, recordID *string, m interface{}) (*servicecatalog.DescribeRecordOutput, diag.Diagnostics) {
 	scconn := m.(*AWSClient).scconn
 
-	var diags diag.Diagnostics
+	var (
+		status *servicecatalog.DescribeRecordOutput
+		diags  diag.Diagnostics
+	)
 
 	record := &servicecatalog.DescribeRecordInput{
 		Id: recordID,
@@ -393,9 +441,10 @@ func waitForProvisioning(name string, recordID *string, m interface{}) diag.Diag
 
 	for {
 		// Get the provisioning status.
-		status, err := scconn.DescribeRecord(record)
+		var err error
+		status, err = scconn.DescribeRecord(record)
 		if err != nil {
-			return diag.Errorf("Error reading provisioning status of account %s: %v", name, err)
+			return status, diag.Errorf("Error reading provisioning status of account %s: %v", name, err)
 		}
 
 		// If the provisioning succeeded we are done.
@@ -405,12 +454,103 @@ func waitForProvisioning(name string, recordID *string, m interface{}) diag.Diag
 
 		// If the provisioning failed we try to cleanup the tainted account.
 		if *status.RecordDetail.Status == servicecatalog.RecordStatusFailed {
-			return diag.Errorf("Provisioning account %s failed: %s", name, *status.RecordDetail.RecordErrors[0].Description)
+			return status, diag.Errorf("Provisioning account %s failed: %s", name, *status.RecordDetail.RecordErrors[0].Description)
 		}
 
 		// Wait 5 seconds before checking the status again.
 		time.Sleep(5 * time.Second)
 	}
 
-	return diags
+	return status, diags
+}
+
+func toOrganizationsTags(tags map[string]interface{}) []*organizations.Tag {
+	result := make([]*organizations.Tag, 0, len(tags))
+
+	for k, v := range tags {
+		tag := &organizations.Tag{
+			Key:   aws.String(k),
+			Value: aws.String(v.(string)),
+		}
+
+		result = append(result, tag)
+	}
+
+	return result
+}
+
+func fromOrganizationTags(tags []*organizations.Tag) map[string]*string {
+	m := make(map[string]*string, len(tags))
+
+	for _, tag := range tags {
+		m[aws.StringValue(tag.Key)] = tag.Value
+	}
+
+	return m
+}
+
+func updateAccountTags(conn *organizations.Organizations, identifier string, oldTags interface{}, newTags interface{}) error {
+	oldTagsMap := oldTags.(map[string]interface{})
+	newTagsMap := newTags.(map[string]interface{})
+
+	if removedTags := removedTags(oldTagsMap, newTagsMap); len(removedTags) > 0 {
+		input := &organizations.UntagResourceInput{
+			ResourceId: aws.String(identifier),
+			TagKeys:    aws.StringSlice(keys(removedTags)),
+		}
+
+		_, err := conn.UntagResource(input)
+
+		if err != nil {
+			return fmt.Errorf("error untagging resource (%s): %w", identifier, err)
+		}
+	}
+
+	if updatedTags := updatedTags(oldTagsMap, newTagsMap); len(updatedTags) > 0 {
+		input := &organizations.TagResourceInput{
+			ResourceId: aws.String(identifier),
+			Tags:       toOrganizationsTags(updatedTags),
+		}
+
+		_, err := conn.TagResource(input)
+
+		if err != nil {
+			return fmt.Errorf("error tagging resource (%s): %w", identifier, err)
+		}
+	}
+
+	return nil
+}
+
+func removedTags(oldTagsMap map[string]interface{}, newTagsMap map[string]interface{}) map[string]interface{} {
+	result := map[string]interface{}{}
+
+	for k, v := range oldTagsMap {
+		if _, ok := newTagsMap[k]; !ok {
+			result[k] = v
+		}
+	}
+
+	return result
+}
+
+func updatedTags(oldTagsMap map[string]interface{}, newTagsMap map[string]interface{}) map[string]interface{} {
+	result := map[string]interface{}{}
+
+	for k, newV := range newTagsMap {
+		if oldV, ok := oldTagsMap[k]; !ok || oldV != newV {
+			result[k] = newV
+		}
+	}
+
+	return result
+}
+
+func keys(value map[string]interface{}) []string {
+	keys := make([]string, 0, len(value))
+	for k := range value {
+		keys = append(keys, k)
+	}
+
+	return keys
 }
